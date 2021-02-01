f8cca7555087f1dbb45bc86f74e0c39161697f4c
Arthur LE RAY <contact.arthurleray@gmail.com>
2021-02-01 11:56:59 +0100

    Added inputs to form

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -17,79 +17,21 @@
       </template>
       <v-card>
         <v-card-title>
-          <span class="headline">User Profile</span>
+          <span class="headline">Créer une nouvelle tâche</span>
         </v-card-title>
         <v-card-text>
           <v-container>
             <v-row>
-              <v-col
-                  cols="12"
-                  sm="6"
-                  md="4"
-              >
-                <v-text-field
-                    label="Legal first name*"
-                    required
-                ></v-text-field>
-              </v-col>
-              <v-col
-                  cols="12"
-                  sm="6"
-                  md="4"
-              >
-                <v-text-field
-                    label="Legal middle name"
-                    hint="example of helper text only on focus"
-                ></v-text-field>
-              </v-col>
-              <v-col
-                  cols="12"
-                  sm="6"
-                  md="4"
-              >
-                <v-text-field
-                    label="Legal last name*"
-                    hint="example of persistent helper text"
-                    persistent-hint
-                    required
-                ></v-text-field>
-              </v-col>
               <v-col cols="12">
-                <v-text-field
-                    label="Email*"
-                    required
-                ></v-text-field>
+                <v-text-field label="Titre de la tâche" required v-model="taskTitle"></v-text-field>
               </v-col>
+            </v-row>
+            <v-row>
               <v-col cols="12">
-                <v-text-field
-                    label="Password*"
-                    type="password"
-                    required
-                ></v-text-field>
-              </v-col>
-              <v-col
-                  cols="12"
-                  sm="6"
-              >
-                <v-select
-                    :items="['0-17', '18-29', '30-54', '54+']"
-                    label="Age*"
-                    required
-                ></v-select>
-              </v-col>
-              <v-col
-                  cols="12"
-                  sm="6"
-              >
-                <v-autocomplete
-                    :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                    label="Interests"
-                    multiple
-                ></v-autocomplete>
+                <v-textarea label="Description de la tâche" counter="60" required v-model="taskDesc"></v-textarea>
               </v-col>
             </v-row>
           </v-container>
-          <small>*indicates required field</small>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
@@ -98,14 +40,14 @@
               text
               @click="dialog = false"
           >
-            Close
+            Annuler
           </v-btn>
           <v-btn
               color="blue darken-1"
               text
               @click="dialog = false"
           >
-            Save
+            Sauvegarder
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -118,6 +60,8 @@ export default {
 
   data: () => ({
     dialog: false,
+    taskTitle: "",
+    taskDesc: "",
   }),
 
 }
