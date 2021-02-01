8de92ae6314c31f7dabf4a38639811bd66619f82
Arthur LE RAY <contact.arthurleray@gmail.com>
2021-02-01 11:27:44 +0100

    Added cards

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,22 @@
 <template>
   <v-container>
-    <v-row class="text-center title-wrapper">
+    <v-row class="justify-center title-wrapper">
       <v-col cols="12">
         <h1>Mon projet</h1>
       </v-col>
     </v-row>
+    <v-divider></v-divider>
     <v-row class="col-area text-center">
       <v-col cols="3">
-        <v-card
-        elevation="2">
+        <h2 style="margin-bottom: 15px;">À faire</h2>
 
+        <v-card tile class="inside-card">
+          <v-card-title>Title</v-card-title>
+          <v-card-text>Text</v-card-text>
+        </v-card>
+        <v-card tile class="inside-card">
+          <v-card-title>Title</v-card-title>
+          <v-card-text>Text</v-card-text>
         </v-card>
       </v-col>
       <v-col cols="1"></v-col>
@@ -27,7 +34,7 @@
 
 <script>
 export default {
-name: "Dashboard"
+  name: "Dashboard"
 }
 </script>
 
