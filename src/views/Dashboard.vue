939f430e78ebf4f1338ff08b5da5be80b2b801ce
Arthur LE RAY <contact.arthurleray@gmail.com>
2021-02-01 11:12:37 +0100

    Added fake login

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <v-row class="text-center title-wrapper">
+      <v-col cols="12">
+        <h1>Mon projet</h1>
+      </v-col>
+    </v-row>
+    <v-row class="col-area text-center">
+      <v-col cols="3">
+        <v-card
+        elevation="2">
+
+        </v-card>
+      </v-col>
+      <v-col cols="1"></v-col>
+      <v-col cols="3">
+        Test
+      </v-col>
+      <v-col cols="1"></v-col>
+      <v-col cols="3">
+        Test
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+name: "Dashboard"
+}
+</script>
+
+<style scoped>
+
+</style>
